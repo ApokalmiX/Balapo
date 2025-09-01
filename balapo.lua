a031e4f300de4f102ab45728dc51cf4238205ba7
Tristan Laget <tristan.laget@agileo-automation.com>
2025-09-01 09:44:26 +0200

    feat(config): introduce config page
    - WIP: new joker pack
    - new voucher

--- balapo.lua
+++ balapo.lua
@@ -1,1 +1,34 @@
-SMODS.load_file("src/jokers_1.lua")()
+local config = SMODS.current_mod.config
+
+SMODS.current_mod.config_tab = function()
+    return {
+        n = G.UIT.ROOT,
+        config = {
+          align = "cm",
+          padding = 0.05,
+          colour = G.C.CLEAR,
+        },
+        nodes = {
+          create_toggle({
+              label = "Jokers pack 1 (restart required)",
+              ref_table = config,
+              ref_value = "joker_pack_1",
+          }),
+          create_toggle({
+              label = "Jokers pack 2 (restart required)",
+              ref_table = config,
+              ref_value = "joker_pack_2",
+          })
+        },
+      }
+end
+
+if config.joker_pack_1 then
+    SMODS.load_file("src/jokers_1.lua")()
+end
+
+if config.joker_pack_2 then
+    SMODS.load_file("src/jokers_2.lua")()
+end
+
+SMODS.load_file("src/vouchers.lua")()
---
+++ config.lua
@@ -0,0 +1,4 @@
+return {
+	["joker_pack_1"] = true,
+	["joker_pack_2"] = true
+}
